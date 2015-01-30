3781bfe489cf460e775a6906eeb5bcf78021872d
ewingcode <fiafa5000@163.com>
2015-01-30 16:17:12 +0800

    modify

---
+++ src/com/admin/action/RelResAction.java
@@ -0,0 +1,189 @@
+package com.admin.action;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.axis.utils.StringUtils;
+import org.apache.log4j.Logger;
+
+import com.admin.constant.SysParamCode;
+import com.admin.model.SysParam;
+import com.admin.model.WebRelResource;
+import com.admin.model.WebResource;
+import com.admin.service.RelResService;
+import com.core.app.action.base.BaseAction;
+import com.core.app.action.base.ResponseData;
+import com.core.app.action.base.ResponseUtils;
+import com.core.jdbc.util.PageBean;
+
+/**
+ * 分类和资源关系展示类
+ * 
+ * @author tanson lam
+ * @creation 2015年1月29日
+ */
+public class RelResAction extends BaseAction {
+
+	private static Logger logger = Logger.getLogger(ResAction.class);
+	private static final String LIST_PAGE = "/admin/relres/relreslist.html";
+	private static final String EDIT_FORM = "/admin/relres/relresform.html";
+
+	@Resource
+	private RelResService relResService;
+
+	/**
+	 * 查詢列表
+	 */
+	public void show() {
+		try {
+			Map<String, Object> dataModel = new HashMap<String, Object>();
+			String pageStr = request.getParameter("page");
+			String pageSizeStr = request.getParameter("pageSize");
+			Integer page = StringUtils.isEmpty(pageStr) ? null : Integer
+					.valueOf(pageStr);
+			Integer pageSize = StringUtils.isEmpty(pageSizeStr) ? null
+					: Integer.valueOf(pageSizeStr);
+			String condition = bulidConditionSql();
+			PageBean pageBean = baseModelService.pageQuery(condition,
+					bulidOrderBySql(), pageSize, page, WebRelResource.class);
+			List<SysParam> iseffCode = sysParamService
+					.getSysParam(SysParamCode.ISEFF);
+			dataModel.put("iseffCode", iseffCode);
+			dataModel.put("pageBean", pageBean);
+			dataModel.put("pageUrl",
+					getPaginationUrl("/Admin-RelRes-show.action"));
+
+			render(LIST_PAGE, dataModel);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * 显示编辑表单
+	 */
+	public void showEditForm() {
+		Map<String, Object> dataModel = new HashMap<String, Object>();
+		try {
+			String id = request.getParameter("id");
+			if (!StringUtils.isEmpty(id)) {
+				WebRelResource relResource = findOne(Integer.valueOf(id),
+						WebRelResource.class);
+				dataModel.put("bean", relResource);
+			}
+			List<SysParam> iseffCode = sysParamService
+					.getSysParam(SysParamCode.ISEFF);
+			dataModel.put("iseffCode", iseffCode);
+			render(EDIT_FORM, dataModel);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * 保存导航栏
+	 */
+	public void save() {
+		ResponseData responseData = null;
+		try {
+			String id = request.getParameter("id");
+			WebRelResource relResource = new WebRelResource();
+			this.buildPageData(relResource);
+			if (!StringUtils.isEmpty(id)) {
+				relResource.setId(Integer.valueOf(id));
+				baseModelService.update(relResource);
+			} else {
+				baseModelService.save(relResource);
+			}
+			responseData = ResponseUtils.success("保存成功！");
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			responseData = ResponseUtils.fail("保存失败！");
+		}
+		this.outResult(responseData);
+	}
+
+	/**
+	 * 删除导航栏
+	 */
+	public void delete() {
+		ResponseData responseData = null;
+		try {
+			String selectItems = request.getParameter("selectItems");
+			if (selectItems.isEmpty()) {
+				responseData = ResponseUtils.fail("没有选中的数据！");
+				this.outResult(responseData);
+				return;
+			}
+			String[] selectArr = selectItems.split(",");
+			for (String id : selectArr) {
+				WebRelResource relResource = baseModelService.findOne(
+						Integer.valueOf(id), WebRelResource.class);
+				if (relResource != null)
+					baseModelService.delete(relResource);
+			}
+			responseData = ResponseUtils.success("删除成功！");
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			responseData = ResponseUtils.fail("删除失败！");
+		}
+		this.outResult(responseData);
+	}
+
+	/**
+	 * 绑定分类和资源的关系
+	 */
+	public void bindRelResource() {
+		ResponseData responseData = null;
+		try {
+			String resourceIdsStr = request.getParameter("resourceIds");
+			Integer[] resourceIds = null;
+			if (!StringUtils.isEmpty(resourceIdsStr)) {
+				String[] resourceIdArr = resourceIdsStr.split(",");
+				resourceIds = Arrays.asList(resourceIdArr).toArray(
+						new Integer[resourceIdArr.length]);
+			}
+			WebRelResource relResource = new WebRelResource();
+			this.buildPageData(relResource);
+			relResService.bindRelResource(relResource.getCategoryId(),
+					resourceIds, relResource.getTempalte(),
+					relResource.getIseff());
+			responseData = ResponseUtils.success("成功绑定分类和资源关系！");
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			responseData = ResponseUtils.fail("绑定失败！");
+		}
+		this.outResult(responseData);
+	}
+
+	/**
+	 * 解开分类和资源的关系
+	 */
+	public void unBindRelResource() {
+		ResponseData responseData = null;
+		try {
+			String resourceIdsStr = request.getParameter("resourceIds");
+			String categoryIdStr = request.getParameter("categoryId");
+			Integer[] resourceIds = null;
+			Integer categoryId = null;
+			if (!StringUtils.isEmpty(resourceIdsStr)) {
+				String[] resourceIdArr = resourceIdsStr.split(",");
+				resourceIds = Arrays.asList(resourceIdArr).toArray(
+						new Integer[resourceIdArr.length]);
+			}
+			if (!StringUtils.isEmpty(categoryIdStr))
+				categoryId = Integer.valueOf(categoryIdStr);
+			relResService.unBindRelResource(categoryId, resourceIds);
+			responseData = ResponseUtils.success("成功解除分类和资源关系！");
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			responseData = ResponseUtils.fail("解除失败！");
+		}
+		this.outResult(responseData);
+	}
+
+}
---
+++ src/com/admin/service/RelResService.java
@@ -0,0 +1,91 @@
+package com.admin.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Repository;
+
+import com.admin.dto.RelResourceDto;
+import com.admin.model.WebRelResource;
+import com.core.app.service.BaseModelService;
+import com.core.jdbc.DaoException;
+import com.core.jdbc.util.PageBean;
+import com.util.SqlUtil;
+
+/**
+ * 分类和资源关系服务类
+ * 
+ * @author tanson lam
+ * @creation 2015年1月29日
+ */
+@Repository("relResService")
+public class RelResService {
+	@Resource
+	public BaseModelService baseModelService;
+
+	/**
+	 * 查询某一分类的关联资源
+	 * 
+	 * @param categoryId
+	 * @param pageSize
+	 * @param page
+	 * @return
+	 * @throws DaoException
+	 */
+	public PageBean listRelResourceByCategory(Integer categoryId,
+			Integer pageSize, Integer page) throws DaoException { 
+		String sql = "SELECT "
+				+ " r.`category_id` AS categoryId, c.name  AS categoryName ,"
+				+ " r.`resource_id` AS resourceId, s.`name` AS resourceName,"
+				+ " s.`iseff` AS resourceIseff,"
+				+ " r.`create_time` AS createTime, r.`last_update` AS lastUpdate"
+				+ " FROM web_rel_resource r ,web_resource s , web_category c "
+				+ " WHERE "
+				+ " r.`category_id` = c.`id` AND r.`resource_id`=s.`id` AND "
+				+ " r.`category_id`=" + categoryId;
+		return baseModelService.executePageQuery(sql, pageSize, page,
+				RelResourceDto.class);
+	}
+
+	/**
+	 * 绑定分类和资源关系
+	 * 
+	 * @param categoryId
+	 *            分类id
+	 * @param resourceIds
+	 *            多个资源id
+	 * @param template
+	 *            页面模板
+	 * @param iseff
+	 *            是否有效
+	 * @throws DaoException
+	 */
+	public void bindRelResource(Integer categoryId, Integer[] resourceIds,
+			String template, String iseff) throws DaoException {
+		for (Integer resourceId : resourceIds) {
+			WebRelResource relResource = new WebRelResource();
+			relResource.setCategoryId(categoryId);
+			relResource.setIseff(iseff);
+			relResource.setResourceId(resourceId);
+			relResource.setTempalte(template);
+			baseModelService.save(relResource);
+		}
+	}
+
+	/**
+	 * 删除分类和资源关系
+	 * 
+	 * @param categoryId
+	 *            分类id
+	 * @param resourceIds
+	 *            多个资源id
+	 * @throws DaoException
+	 */
+	public void unBindRelResource(Integer categoryId, Integer[] resourceIds)
+			throws DaoException {
+		String sql = "delete from web_rel_resource where category_id= "
+				+ categoryId + " and  resource_id in ("
+				+ SqlUtil.array2InCondition(resourceIds) + ")";
+		baseModelService.executeSql(sql);
+	}
+
+}
